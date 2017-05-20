127935216b63002cd121ca2aa6e6a70bd0c3c0de
Wan Xiaolin <macrocosmist@gmail.com>
2017-05-20 10:40:38 +0800

    Update predict.m

--- model/cnn/predict.m
+++ model/cnn/predict.m
@@ -1,8 +1,16 @@
 function [ pred ] = predict( bu1_81, class_name )
-    %PREDICT 
-    pred.cls_prob = 0;
-    pred.cls_id   = 0;
-    pred.result   = '';
+    %PREDICT Generate prediction according to the output of fc_8.
+    %   Args:
+    %     bu1_81: The output of fc_8 (full-connected layer 8)
+    %     class_name: A cell array of class name
+    %   Returns:
+    %     pred:
+    %
+    %   Author: Wan Xiaolin <wanxl13@lzu.edu.cn>
+    
+    pred.cls_prob = 0;  % Class probability 
+    pred.cls_id   = 0;  % Class ID
+    pred.result   = ''; % Output string of classification results
     
     [pred.cls_prob, pred.cls_id] = max(softmax(bu1_81));
     pred.result = [ ...
