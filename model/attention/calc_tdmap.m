40962492a6e449011f2f3bd369a649417087a71f
Wan Xiaolin <macrocosmist@gmail.com>
2017-06-14 11:02:43 +0800

    update calc_tdmap.m

--- model/attention/calc_tdmap.m
+++ model/attention/calc_tdmap.m
@@ -1,7 +1,7 @@
-function [ td_map ] = calc_tdmap( g, bu, W_, hp )
+function [ td_map ] = calc_tdmap( td, bu, W, hp )
     %GETTDMAP Calculate top-down saliency map on the given parameters.
     %   Args:
-    %     g: Top-down signal.
+    %     td: Top-down signal.
     %     bu: Bottom-up saliency map.
     %     W: NN weights, conv kernel or full-connected layer weights.
     %     hp: Hyperparamters.
@@ -10,21 +10,19 @@ function [ td_map ] = calc_tdmap( g, bu, W_, hp )
     %
     %   Author: Wan Xiaolin <wanxl13@lzu.edu.cn>
     
-    W_ = max(W_, 0);
-    if ismatrix(W_) % Full-connected layer
-        W = W_;
+    W = max(W, 0);
+    if ismatrix(W)  % Full-connected layer
         X = W * bu;
-        %Y = compete(g, hp) ./ X;
-        Y = g ./ X;
+        Y = td ./ X;
         Z = W' * Y;
         td_map = bu .* Z;
-    else % Convolutional layer
-        W = krnl2col(W_);
-        WT = krnl2col(permute(W_, [1, 2, 4, 3]));
+    else  % Convolutional layer
+        WT = krnl2col(permute(W, [1, 2, 4, 3]));
+        W = krnl2col(W);
         bu_col = im2col(padding(bu));
         X = W * bu_col;
         X = col2im(X);
-        Y = compete(g, hp) ./ X; 
+        Y = compete(td, hp) ./ X; 
         Y = im2col(padding(Y));
         Z = WT * Y;
         Z = col2im(Z);
